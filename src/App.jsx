66f15226686b9cbf0b4ee3b2fef34209024559a9
Jakub <kuba2092001@interia.pl>
2025-05-27 19:21:11 +0200

    Add roll button and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import Die from './components/Die';
 function App() {
-  const diceElements = Array.from({ length: 10 }).map((_, index) => {
-    const randomValue = Math.floor(Math.random() * 6 + 1);
-    return <Die key={index} value={randomValue} />;
-  });
+  const [dice, setDice] = useState(() => generateAllNewDice());
+
+  function getRandomValue() {
+    return Math.floor(Math.random() * 6 + 1);
+  }
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => getRandomValue());
+  }
 
   return (
     <main>
-      <div className="dice-container">{diceElements}</div>
+      <div className="dice-container">
+        {dice.map((value, index) => (
+          <Die key={index} value={value} />
+        ))}
+      </div>
+      <button className="roll-btn" onClick={() => setDice(generateAllNewDice)}>
+        Roll dice
+      </button>
     </main>
   );
 }

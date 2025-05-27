345b301fc3ac08fc7ee2a4a880a4216b531d5a48
Jakub <kuba2092001@interia.pl>
2025-05-27 18:07:57 +0200

    Render 10 dice with random values

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Die from './components/Die';
 function App() {
-  return <main>Hey I'm app component!</main>;
+  const diceElements = Array.from({ length: 10 }).map((_, index) => {
+    const randomValue = Math.floor(Math.random() * 6 + 1);
+    return <Die key={index} value={randomValue} />;
+  });
+
+  return (
+    <main>
+      <div className="buttons">{diceElements}</div>
+    </main>
+  );
 }
 
 export default App;
